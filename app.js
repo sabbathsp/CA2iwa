3f0013780a01561443ff4170431f4ccd67843b29
sabbathsp <sabbathsp@hotmail.com>
2020-04-25 16:56:44 +0000

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express') //This module allows this app to respond to HTTP Requests, defines the routing and renders back the required content
 
 const app = express();
+const mongoose = require( 'mongoose');
+require('dotenv/config');
+// require('dotenv').config();
+
 
 // routes
 app.get('/', (req, res) => {
     res.send ('Lets do It');
 
 });
+
+//connect to db
+// mongoose.connect('mongodb+srv://test:cctca2iwa@cluster0-nnxv8.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
+});
+
+
 
 // listening the server
 
